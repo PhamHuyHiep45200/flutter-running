727004ef4d6f27f9d3825e4c03b78b95b4572f10
Huy Hiep <hiepph3@rikkeisoft.com>
2025-10-21 16:49:45 +0700

    map

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -9,6 +9,8 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final List<int> numbers = [1, 2, 3, 4, 5];
+  int count = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,89 +18,112 @@ class _LoginScreenState extends State<LoginScreen> {
         body: Center(
           child: Container(
             width: 300,
-            height: 300,
+            height: 600,
             color: Colors.white,
-            child: SfRadialGauge(
-              axes: <RadialAxis>[
-                RadialAxis(
-                  minimum: 0,
-                  maximum: 40,
-                  interval: 2,
-                  labelFormat: '',
-                  labelsPosition: ElementsPosition.outside,
-                  axisLineStyle: const AxisLineStyle(
-                    thickness: 30,
-                    color: Color.fromARGB(179, 240, 240, 240),
-                  ),
-                  showTicks: false,
-                ),
-                RadialAxis(
-                  minimum: 0,
-                  maximum: 40,
-                  interval: 2,
-                  labelFormat: '',
-                  labelsPosition: ElementsPosition.outside,
-                  ranges: <GaugeRange>[
-                    GaugeRange(
-                      startValue: 0,
-                      endValue: 30,
-                      color: Colors.blueAccent,
-                      startWidth: 30,
-                      endWidth: 30,
+            child: Column(
+              children: [
+                SfRadialGauge(
+                  axes: <RadialAxis>[
+                    RadialAxis(
+                      minimum: 0,
+                      maximum: 40,
+                      interval: 2,
+                      labelFormat: '',
+                      labelsPosition: ElementsPosition.outside,
+                      axisLineStyle: const AxisLineStyle(
+                        thickness: 30,
+                        color: Color.fromARGB(179, 240, 240, 240),
+                      ),
+                      showTicks: false,
                     ),
-                  ],
-                  axisLineStyle: const AxisLineStyle(
-                    thickness: 45,
-                    color: Colors.transparent,
-                  ),
+                    RadialAxis(
+                      minimum: 0,
+                      maximum: 40,
+                      interval: 2,
+                      labelFormat: '',
+                      labelsPosition: ElementsPosition.outside,
+                      ranges: <GaugeRange>[
+                        GaugeRange(
+                          startValue: 0,
+                          endValue: 30,
+                          color: Colors.blueAccent,
+                          startWidth: 30,
+                          endWidth: 30,
+                        ),
+                      ],
+                      axisLineStyle: const AxisLineStyle(
+                        thickness: 45,
+                        color: Colors.transparent,
+                      ),
 
-                  // pointers: <GaugePointer>[NeedlePointer(value: 30,)],
-                  annotations: <GaugeAnnotation>[
-                    GaugeAnnotation(
-                      widget: Container(
-                        width: 32,
-                        height: 32,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(6),
-                          border: Border.all(color: Colors.blueAccent, width: 3)
-                        ),
-                        child: Center(
-                          child: Text(
-                            '0',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
+                      // pointers: <GaugePointer>[NeedlePointer(value: 30,)],
+                      annotations: <GaugeAnnotation>[
+                        GaugeAnnotation(
+                          widget: Container(
+                            width: 32,
+                            height: 32,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(6),
+                              border: Border.all(
+                                color: Colors.blueAccent,
+                                width: 3,
+                              ),
+                            ),
+                            child: Center(
+                              child: Text(
+                                '0',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
                             ),
                           ),
+                          positionFactor: 0.74,
+                          angle: 130,
                         ),
-                      ),
-                      positionFactor: 0.74,
-                      angle: 130,
-                    ),
-                    GaugeAnnotation(
-                      widget: Container(
-                        width: 32,
-                        height: 32,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(6),
-                          border: Border.all(color: Colors.blueGrey, width: 3)
-                        ),
-                        child: Center(
-                          child: Text(
-                            '40',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
+                        GaugeAnnotation(
+                          widget: Container(
+                            width: 32,
+                            height: 32,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(6),
+                              border: Border.all(
+                                color: Colors.blueGrey,
+                                width: 3,
+                              ),
+                            ),
+                            child: Center(
+                              child: Text(
+                                '40',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
                             ),
                           ),
+                          positionFactor: 0.74,
+                          angle: 50,
                         ),
-                      ),
-                      positionFactor: 0.74,
-                      angle: 50,
+                      ],
                     ),
                   ],
+                ),
+                Row(
+                  children: numbers
+                      .map((e) => Text(e.toString(), key: ValueKey<int>(e)))
+                      .toList(),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      count++;
+                    });
+                  },
+                  child: Text('count $count'),
                 ),
               ],
             ),
