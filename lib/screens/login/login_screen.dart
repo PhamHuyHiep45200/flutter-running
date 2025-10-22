fc26d71d4f97b81bf9d692b4644613b34b1d9b72
Huy Hiep <hiepph3@rikkeisoft.com>
2025-10-22 16:41:13 +0700

    add img

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -22,93 +22,128 @@ class _LoginScreenState extends State<LoginScreen> {
             color: Colors.white,
             child: Column(
               children: [
-                SfRadialGauge(
-                  axes: <RadialAxis>[
-                    RadialAxis(
-                      minimum: 0,
-                      maximum: 40,
-                      interval: 2,
-                      labelFormat: '',
-                      labelsPosition: ElementsPosition.outside,
-                      axisLineStyle: const AxisLineStyle(
-                        thickness: 30,
-                        color: Color.fromARGB(179, 240, 240, 240),
+                Stack(
+                  children: [
+                    Positioned(
+                      bottom: 0,
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      // width: ,
+                      child: SizedBox(
+                        // width: 180,
+                        // height: 180,
+                        child: Center(
+                          child: Image.asset(
+                            'assets/shape.png',
+                            width: 218,
+                            height: 218,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
-                      showTicks: false,
                     ),
-                    RadialAxis(
-                      minimum: 0,
-                      maximum: 40,
-                      interval: 2,
-                      labelFormat: '',
-                      labelsPosition: ElementsPosition.outside,
-                      ranges: <GaugeRange>[
-                        GaugeRange(
-                          startValue: 0,
-                          endValue: 30,
-                          color: Colors.blueAccent,
-                          startWidth: 30,
-                          endWidth: 30,
-                        ),
-                      ],
-                      axisLineStyle: const AxisLineStyle(
-                        thickness: 45,
-                        color: Colors.transparent,
-                      ),
-
-                      // pointers: <GaugePointer>[NeedlePointer(value: 30,)],
-                      annotations: <GaugeAnnotation>[
-                        GaugeAnnotation(
-                          widget: Container(
-                            width: 32,
-                            height: 32,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(6),
-                              border: Border.all(
+                    Positioned(
+                      child: SfRadialGauge(
+                        axes: <RadialAxis>[
+                          RadialAxis(
+                            minimum: 0,
+                            maximum: 40,
+                            interval: 2,
+                            labelFormat: '',
+                            labelsPosition: ElementsPosition.outside,
+                            axisLineStyle: const AxisLineStyle(
+                              thickness: 30,
+                              color: Color.fromARGB(179, 240, 240, 240),
+                            ),
+                            showTicks: false,
+                          ),
+                          RadialAxis(
+                            minimum: 0,
+                            maximum: 40,
+                            interval: 2,
+                            labelFormat: '',
+                            labelsPosition: ElementsPosition.outside,
+                            ranges: <GaugeRange>[
+                              GaugeRange(
+                                startValue: 0,
+                                endValue: 30,
                                 color: Colors.blueAccent,
-                                width: 3,
+                                startWidth: 30,
+                                endWidth: 30,
                               ),
+                            ],
+                            axisLineStyle: const AxisLineStyle(
+                              thickness: 45,
+                              color: Colors.transparent,
                             ),
-                            child: Center(
-                              child: Text(
-                                '0',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
+
+                            // pointers: <GaugePointer>[NeedlePointer(value: 30,)],
+                            annotations: <GaugeAnnotation>[
+                              // GaugeAnnotation(
+                              //   widget: SizedBox(
+                              //     width: 200,
+                              //     height: 200,
+                              //     child: Center(
+                              //       child: Image.asset('assets/shape.png', fit: BoxFit.cover,)
+                              //     ),
+                              //   ),
+                              //   angle: 0,
+                              // ),
+                              GaugeAnnotation(
+                                widget: Container(
+                                  width: 32,
+                                  height: 32,
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(6),
+                                    border: Border.all(
+                                      color: Colors.blueAccent,
+                                      width: 3,
+                                    ),
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      '0',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
                                 ),
+                                positionFactor: 0.74,
+                                angle: 130,
                               ),
-                            ),
-                          ),
-                          positionFactor: 0.74,
-                          angle: 130,
-                        ),
-                        GaugeAnnotation(
-                          widget: Container(
-                            width: 32,
-                            height: 32,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(6),
-                              border: Border.all(
-                                color: Colors.blueGrey,
-                                width: 3,
-                              ),
-                            ),
-                            child: Center(
-                              child: Text(
-                                '40',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
+                              GaugeAnnotation(
+                                widget: Container(
+                                  width: 32,
+                                  height: 32,
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(6),
+                                    border: Border.all(
+                                      color: Colors.blueGrey,
+                                      width: 3,
+                                    ),
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      '40',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
                                 ),
+                                positionFactor: 0.74,
+                                angle: 50,
                               ),
-                            ),
+                            ],
                           ),
-                          positionFactor: 0.74,
-                          angle: 50,
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ],
                 ),
